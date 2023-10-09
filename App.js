46e7cf6e7ccf55d4a5c2e1ce98069e3e71b3bd5b
Stefan Manic <stefanmanic@StefanManic-macbook.local>
2023-10-09 08:24:08 +0200

    Add some screens, work on Layout and Styling

--- App.js
+++ App.js
@@ -1,20 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import WelcomeScreen from "./app/screens/WelcomeScreen";
+import ViewImageScreen from "./app/screens/ViewImageScreen";
+import AppText from "./app/components/AppText";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import AppButton from "./app/components/AppButton";
+import Card from "./app/components/Card";
+import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
+import MessageScreen from "./app/components/MessageScreen";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    // <MessageScreen />
+    <ViewImageScreen />
+    // <ListingDetailsScreen />
+    // <View
+    //   style={{
+    //     flex: 1,
+    //     backgroundColor: "#f8f4f4",
+    //     padding: 20,
+    //     paddingTop: 50,
+    //   }}
+    // >
+    //   <Card
+    //     image={require("./app/assets/jacket.jpg")}
+    //     title={"Black jacket for sale"}
+    //     subTitle={"$100"}
+    //   />
+    // </View>
+    // <WelcomeScreen />
+    // <View style={styles.container}>
+    //   {/* <AppText>I love React Native</AppText>
+    //   <MaterialCommunityIcons name="email" size={100} /> */}
+    //   <AppButton title={"Login"} onPress={() => console.log("Tapped")} />
+    // </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#4ecdc4",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ app/screens/WelcomeScreen.js
@@ -0,0 +1,58 @@
+import React from "react";
+import {
+  Image,
+  ImageBackground,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
+import AppButton from "../components/AppButton";
+
+function WelcomeScreen() {
+  return (
+    <ImageBackground
+    blurRadius={10}
+      style={styles.background}
+      source={require("../assets/background.jpg")}
+    >
+      <View style={styles.logoContainer}>
+        <Image style={styles.logo} source={require("../assets/logo.jpg")} />
+        <Text style={styles.tagline}>Sell What You Don't Need</Text>
+      </View>
+      <View style={styles.buttonContainer}>
+        <AppButton title={"Login"} />
+        <AppButton title={"registration"} color={"secondary"} />
+      </View>
+    </ImageBackground>
+  );
+}
+
+const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    justifyContent: "flex-end",
+    alignItems: "center",
+  },
+  buttonContainer: {
+    padding: 20,
+    width: "100%",
+  },
+  logoContainer: {
+    position: "absolute",
+    top: 70,
+    alignItems: "center",
+  },
+  logo: {
+    width: 220,
+    height: 80,
+  },
+  tagline: {
+    fontSize: 25,
+    fontWeight: "600",
+    paddingVertical: 20,
+    color: "#fc5c65",
+  },
+});
+
+export default WelcomeScreen;
